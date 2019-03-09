ab9b5c00004b3b36c6ba27ad6a1dcd9ab51095fb
Andrew Rabert <ar@nullsum.net>
2019-03-09 15:07:50 -0500

    Remove setuptools_scm Breaks tag archives without resorting to hacks or more setup dependencies.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pipns',
+    version='0.0.5',
     author='Andrew Rabert',
     author_email='ar@nullsum.net',
     url='https://github.com/nvllsvm/pipns',
@@ -10,6 +11,4 @@ setuptools.setup(
     entry_points={'console_scripts': ['pipns=pipns:main']},
     package_data={'pipns': ['scripts/*.py']},
     install_requires=['pipenv'],
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True,
     python_requires='>=3.7')
